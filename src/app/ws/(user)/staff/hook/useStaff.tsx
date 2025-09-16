1aedec8745f02086d5a5a4844b8341494cf811b7
surafelhabte1 <surafelhabte1@gmail.com>
2025-09-16 16:48:47 +0300

    staff

---
+++ src/app/ws/(user)/staff/hook/useStaff.tsx
@@ -0,0 +1,71 @@
+import UserProfileInfo from "@/components/common/UserProfileInfo";
+import { Icon } from "@iconify-icon/react";
+
+export const useStaff = () => {
+  const getTableColumns = (): any[] => {
+    return [
+      {
+        title: "Full Name",
+        dataIndex: "staff",
+        key: "staff",
+        render: (_: string, record: any) => (
+          <UserProfileInfo
+            first_name={record?.first_name}
+            last_name={record?.last_name}
+            photoUrl={record?.photoUrl}
+            link={`/ws/staff-detail/${record?.id}`}
+          />
+        ),
+      },
+      {
+        title: "Gender",
+        dataIndex: "gender",
+        key: "gender",
+        render: (val: string) => <span className="text-sm">{val || "-"}</span>,
+      },
+
+      {
+        title: "Phone #",
+        dataIndex: "phone",
+        key: "phone",
+        render: (_: string, record: any) => (
+          <div className="flex items-center gap-2">
+            <Icon
+              icon="mdi:phone"
+              width={20}
+              height={20}
+              className="text-gray-700"
+            />
+            <span className="text-sm">{record?.phone || "-"}</span>
+          </div>
+        ),
+      },
+      {
+        title: "Email",
+        dataIndex: "email",
+        key: "email",
+        render: (val: string) => (
+          <div className="flex items-center gap-2">
+            <Icon
+              icon="mdi:email"
+              width={20}
+              height={20}
+              className="text-gray-700"
+            />
+            <span className="text-sm">{val || "-"}</span>
+          </div>
+        ),
+      },
+      {
+        title: "Role",
+        dataIndex: "role",
+        key: "role",
+        render: (val: string) => <span className="text-sm">{val || "-"}</span>,
+      },
+    ];
+  };
+
+  return {
+    getTableColumns,
+  };
+};
--- src/app/ws/(user)/staff/new/page.tsx
+++ src/app/ws/(user)/staff/new/page.tsx
@@ -1,9 +1,81 @@
+"use client";
+
+import { FieldType } from "@/components/forms/FormGenerator";
+import FormSkeleton from "@/components/forms/FormSkeleton";
+import dynamic from "next/dynamic";
+
 export default function Home() {
+  const FormFields = [
+    {
+      name: "first_name",
+      label: "First Name",
+      type: FieldType.Input,
+      placeholder: "Enter first name",
+      rules: [{ required: true, message: "" }],
+    },
+    {
+      name: "last_name",
+      label: "Last Name",
+      type: FieldType.Input,
+      placeholder: "Enter last name",
+      rules: [{ required: true, message: "" }],
+    },
+    {
+      name: "email",
+      label: "Email",
+      type: FieldType.Input,
+      placeholder: "Enter email",
+      rules: [{ required: true, message: "" }],
+    },
+    {
+      name: "phone",
+      label: "Phone Number",
+      type: FieldType.Input,
+      placeholder: "Enter phone number",
+      rules: [{ required: true, message: "" }],
+    },
+
+    {
+      name: "gender",
+      label: "Sex",
+      type: FieldType.Select,
+      placeholder: "Select Sex",
+      options: [
+        { label: "Male", value: "male" },
+        { label: "Female", value: "female" },
+      ],
+      rules: [{ required: true, message: "" }],
+    },
+    {
+      name: "role",
+      label: "Role",
+      type: FieldType.Select,
+      placeholder: "Select role",
+      options: [
+        { label: "Librarian", value: "librarian" },
+        { label: "Accountant", value: "accountant" },
+        { label: "Admin", value: "admin" },
+      ],
+      rules: [{ required: true, message: "" }],
+    },
+  ];
+
+  const FormGenerator = dynamic(
+    () => import("@/components/forms/FormGenerator"),
+    {
+      ssr: false,
+      loading: () => <FormSkeleton />,
+    }
+  );
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>
-        <h1>Staff form</h1>
-      </div>
+    <div className="">
+      <FormGenerator
+        columns={2}
+        fields={FormFields}
+        onSubmit={(values) => console.log("Staff form submitted:", values)}
+        title="Staff Registration Form"
+      />
     </div>
   );
 }
