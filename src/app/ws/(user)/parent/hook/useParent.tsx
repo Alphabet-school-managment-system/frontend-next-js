58f9a337e70e7e12c751e445fca22b5bb92fd030
surafelhabte1 <surafelhabte1@gmail.com>
2025-09-16 16:48:33 +0300

    parent

---
+++ src/app/ws/(user)/parent/hook/useParent.tsx
@@ -0,0 +1,71 @@
+import UserProfileInfo from "@/components/common/UserProfileInfo";
+import { Icon } from "@iconify-icon/react";
+
+export const useParent = () => {
+  const getTableColumns = (): any[] => {
+    return [
+      {
+        title: "Full Name",
+        dataIndex: "parent",
+        key: "parent",
+        render: (_: string, record: any) => (
+          <UserProfileInfo
+            first_name={record?.first_name}
+            last_name={record?.last_name}
+            photoUrl={record?.photoUrl}
+            link={`/ws/parent-detail/${record?.id}`}
+          />
+        ),
+      },
+      {
+        title: "Gender",
+        dataIndex: "gender",
+        key: "gender",
+        render: (val: string) => <span className="text-sm">{val || "-"}</span>,
+      },
+
+      {
+        title: "Phone #",
+        dataIndex: "phone",
+        key: "phone",
+        render: (_: string, record: any) => (
+          <div className="flex items-center gap-2">
+            <Icon
+              icon="mdi:phone"
+              width={20}
+              height={20}
+              className="text-gray-700"
+            />
+            <span className="text-sm">{record?.phone || "-"}</span>
+          </div>
+        ),
+      },
+      {
+        title: "Email",
+        dataIndex: "email",
+        key: "email",
+        render: (val: string) => (
+          <div className="flex items-center gap-2">
+            <Icon
+              icon="mdi:email"
+              width={20}
+              height={20}
+              className="text-gray-700"
+            />
+            <span className="text-sm">{val || "-"}</span>
+          </div>
+        ),
+      },
+      {
+        title: "Address",
+        dataIndex: "address",
+        key: "address",
+        render: (val: string) => <span className="text-sm">{val || "-"}</span>,
+      },
+    ];
+  };
+
+  return {
+    getTableColumns,
+  };
+};
--- src/app/ws/(user)/parent/new/page.tsx
+++ src/app/ws/(user)/parent/new/page.tsx
@@ -1,9 +1,82 @@
+"use client";
+
+import { FieldType } from "@/components/forms/FormGenerator";
+import FormSkeleton from "@/components/forms/FormSkeleton";
+import dynamic from "next/dynamic";
+
 export default function Home() {
+  const FormFields = [
+    {
+      name: "first_name",
+      label: "First Name",
+      type: FieldType.Input,
+      placeholder: "Enter first name",
+      rules: [{ required: true, message: "" }],
+    },
+    {
+      name: "last_name",
+      label: "Last Name",
+      type: FieldType.Input,
+      placeholder: "Enter last name",
+      rules: [{ required: true, message: "" }],
+    },
+    {
+      name: "email",
+      label: "Email",
+      type: FieldType.Input,
+      placeholder: "Enter email",
+      rules: [{ required: true, message: "" }],
+    },
+    {
+      name: "phone",
+      label: "Phone Number",
+      type: FieldType.Input,
+      placeholder: "Enter phone number",
+      rules: [{ required: true, message: "" }],
+    },
+
+    {
+      name: "address",
+      label: "Address",
+      type: FieldType.Textarea,
+      placeholder: "Enter address",
+      rules: [{ required: true, message: "" }],
+    },
+    {
+      name: "note",
+      label: "Note",
+      type: FieldType.Textarea,
+      placeholder: "Add extra note here.",
+    },
+    {
+      name: "gender",
+      label: "Sex",
+      type: FieldType.Select,
+      placeholder: "Select Sex",
+      options: [
+        { label: "Male", value: "male" },
+        { label: "Female", value: "female" },
+      ],
+      rules: [{ required: true, message: "" }],
+    },
+  ];
+
+  const FormGenerator = dynamic(
+    () => import("@/components/forms/FormGenerator"),
+    {
+      ssr: false,
+      loading: () => <FormSkeleton />,
+    }
+  );
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>
-        <h1>Parent form</h1>
-      </div>
+    <div className="">
+      <FormGenerator
+        columns={2}
+        fields={FormFields}
+        onSubmit={(values) => console.log("Parent form submitted:", values)}
+        title="Parent Registration Form"
+      />
     </div>
   );
 }
