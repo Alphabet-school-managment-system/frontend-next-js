a5a5b9b84eaa1dda16bc950055fd18675b0d64c0
surafelhabte1 <surafelhabte1@gmail.com>
2025-09-19 19:30:03 +0300

    school

---
+++ src/app/ws/(setting)/school/hook/useSchool.tsx
@@ -0,0 +1,64 @@
+import { FieldConfig, FieldType } from "@/components/forms/FormGenerator";
+
+export const useSchool = () => {
+  const getTableColumns = (): any[] => {
+    return [
+      {
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+        render: (val: string) => <span className="text-sm">{val || "-"}</span>,
+      },
+      {
+        title: "Address",
+        dataIndex: "address",
+        key: "address",
+        render: (val: string) => <span className="text-sm">{val || "-"}</span>,
+      },
+      {
+        title: "Contact",
+        dataIndex: "contact",
+        key: "contact",
+        render: (val: string) => <span className="text-sm">{val || "-"}</span>,
+      },
+    ];
+  };
+
+  const getFormFields = (): FieldConfig[] => {
+    return [
+      {
+        name: "name",
+        label: "Name",
+        type: FieldType.Input,
+        placeholder: "Enter name",
+        rules: [{ required: true, message: "" }],
+      },
+      {
+        name: "address",
+        label: "Address",
+        type: FieldType.Textarea,
+        placeholder: "1234, Main St, City, Country",
+        rows: 4,
+        rules: [{ required: true, message: "" }],
+      },
+      {
+        name: "contact",
+        label: "Contact",
+        type: FieldType.Textarea,
+        placeholder: "+1234567890,example@example.com, www.example.com",
+        rules: [{ required: true, message: "" }],
+      },
+      {
+        name: "note",
+        label: "Note",
+        type: FieldType.Textarea,
+        placeholder: "additional information about the school",
+      },
+    ];
+  };
+
+  return {
+    getTableColumns,
+    getFormFields,
+  };
+};
