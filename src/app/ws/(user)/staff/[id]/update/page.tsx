9be4e76a7aa16602159208d2d6c95cff0254c1f0
surafelhabte1 <surafelhabte1@gmail.com>
2025-09-19 18:18:02 +0300

    staff

--- src/app/ws/(user)/staff/[id]/update/page.tsx
+++ src/app/ws/(user)/staff/[id]/update/page.tsx
@@ -1,9 +1,51 @@
+import FormSkeleton from "@/components/forms/FormSkeleton";
+import { useApiQuery } from "@/hooks/useApi";
+import { Staff } from "@/types";
+import dynamic from "next/dynamic";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useStudent } from "../../../student/hook/useStudent";
+import { useStaff } from "../../hook/useStaff";
+
 export default function Home() {
+  const { id } = useParams();
+  const { getCommonFormFields } = useStudent();
+  const { getFormFields } = useStaff();
+
+  const [data, setData] = useState<any>(null);
+
+  const { data: result, isLoading } = useApiQuery<Staff>(
+    [],
+    `staff/${id}`,
+    Boolean(id)
+  );
+
+  useEffect(() => {
+    if (result) {
+      const payload: any = { ...result };
+      setData(payload);
+    }
+  }, [result]);
+
+  const FormGenerator = dynamic(
+    () => import("@/components/forms/FormGenerator"),
+    {
+      ssr: false,
+      loading: () => <FormSkeleton />,
+    }
+  );
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>
-        <h1>Staff update form</h1>
-      </div>
+    <div className="">
+      <FormGenerator
+        columns={2}
+        fields={[...getCommonFormFields(), ...getFormFields()]}
+        title="Update Staff Information"
+        apiRoute="staff"
+        data={data}
+        isFetching={isLoading}
+        isCreate={false}
+      />
     </div>
   );
 }
