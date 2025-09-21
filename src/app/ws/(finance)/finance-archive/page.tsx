5041ae6d49c53843c309749dc87b6e7d1a30467d
surafelhabte1 <surafelhabte1@gmail.com>
2025-09-21 19:00:15 +0300

    finance archive

--- src/app/ws/(finance)/finance-archive/page.tsx
+++ src/app/ws/(finance)/finance-archive/page.tsx
@@ -1,9 +1,53 @@
+"use client";
+
+import { useFinanceArchive } from "./hook/useFinanceArchive";
+import dynamic from "next/dynamic";
+import TableSkeleton from "@/components/forms/TableSkeleton";
+import { Select } from "@/components/common/Select";
+import { useState } from "react";
+
+const List = dynamic(() => import("@/components/list/index"), {
+  ssr: false,
+  loading: () => <TableSkeleton />,
+});
+
 export default function Home() {
+  const { getTableColumns } = useFinanceArchive();
+  const [type, setType] = useState("fee");
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>
-        <h1>Finance Summary list</h1>
-      </div>
-    </div>
+    <>
+      <List
+        columns={getTableColumns()}
+        searchByCols={["ay_name"]}
+        searchInputPlaceholderText={
+          "Search by Archive information (academic year)"
+        }
+        route={{
+          api: "finance-summarie",
+          page: "",
+        }}
+        showAddButton={false}
+        FilterOption={
+          <Select
+            data={[
+              {
+                value: "fee",
+                text: "Fees",
+              },
+              {
+                value: "expense",
+                text: "Expenses",
+              },
+            ]}
+            placeholderText="Apply filter"
+            onChange={(value: any) => {
+              setType(value);
+            }}
+            classNames="shadow-none focus:shadow-none outline-none bg-transparent min-w-[150px]"
+          />
+        }
+      />
+    </>
   );
 }
