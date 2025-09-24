342cb561ea8595534b8859dbc0ed467ca01a0c4e
surafelhabte1 <surafelhabte1@gmail.com>
2025-09-24 19:32:08 +0300

    academic year

--- src/app/ws/(setting)/academic-year/page.tsx
+++ src/app/ws/(setting)/academic-year/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+
+import { useAcademicYear } from "./hook/useAcademicYear";
+import dynamic from "next/dynamic";
+import TableSkeleton from "@/components/forms/TableSkeleton";
+
+const List = dynamic(() => import("@/components/list/index"), {
+  ssr: false,
+  loading: () => <TableSkeleton />,
+});
+
 export default function Home() {
+  const { getTableColumns } = useAcademicYear();
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>
-        <h1>Academic year list</h1>
-      </div>
-    </div>
+    <>
+      <List
+        columns={getTableColumns()}
+        searchByCols={["name", "start_date", "end_date"]}
+        searchInputPlaceholderText={
+          "Search by academic year information (name, start & end date)"
+        }
+        route={"academic-year"}
+        addButtonTitle={"Add new Academic Year"}
+        actionPrevilage={{
+          edit: true,
+          delete: true,
+          detail: false,
+        }}
+      />
+    </>
   );
 }
