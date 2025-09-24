e3fe8129701eb301b5767093c641a2d012277adb
surafelhabte1 <surafelhabte1@gmail.com>
2025-09-24 17:39:25 +0300

    class section

--- src/app/ws/(academic)/class-section/page.tsx
+++ src/app/ws/(academic)/class-section/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+
+import { useClassSection } from "./hook/useClassSection";
+import dynamic from "next/dynamic";
+import TableSkeleton from "@/components/forms/TableSkeleton";
+
+const List = dynamic(() => import("@/components/list/index"), {
+  ssr: false,
+  loading: () => <TableSkeleton />,
+});
+
 export default function Home() {
+  const { getTableColumns } = useClassSection();
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>
-        <h1>Class Section list</h1>
-      </div>
-    </div>
+    <>
+      <List
+        columns={getTableColumns()}
+        searchByCols={["class_name"]}
+        searchInputPlaceholderText={"Search by class name"}
+        route={"class-section"}
+        addButtonTitle={"Add new Class Sec"}
+        actionPrevilage={{
+          edit: true,
+          delete: true,
+          detail: false,
+        }}
+      />
+    </>
   );
 }
