fae7a04fbabaa9dc068ae7c1f4a73540ecaebe9f
surafelhabte1 <surafelhabte1@gmail.com>
2025-11-11 15:27:10 +0300

    feat: add static images for no-photo placeholders and school icon and export tems in module

---
+++ src/lib/static-images.ts
@@ -0,0 +1,11 @@
+import logoImg from "@/assets/images/logo.png";
+import schoolImg from "@/assets/images/school.png";
+import noPhotoBoyImg from "@/assets/images/no-photo-boy.png";
+import noPhotoGirlImg from "@/assets/images/no-photo-girl.png";
+
+export const staticImages = {
+  logoImg,
+  schoolImg,
+  noPhotoBoyImg,
+  noPhotoGirlImg,
+};
