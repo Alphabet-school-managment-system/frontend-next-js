c99d5c5b6ac7c00e9c5e29d0103834db5631348c
surafelhabte1 <surafelhabte1@gmail.com>
2025-12-22 14:29:09 +0300

    crud test : parent

--- src/app/ws/(user)/parent/hook/useParent.tsx
+++ src/app/ws/(user)/parent/hook/useParent.tsx
@@ -1,29 +1,30 @@
 import UserProfileInfo from "@/components/common/UserProfileInfo";
 import { FieldConfig, FieldType } from "@/components/forms/FormGenerator";
 import { Icon } from "@iconify-icon/react";
+import { get_formatted_sex } from "../../student/hook/useStudent";
+import { Sex } from "@/types/enums";
 
 export const useParent = () => {
   const getTableColumns = (): any[] => {
     return [
       {
-        title: "Full Name",
+        title: "Name",
         dataIndex: "parent",
         key: "parent",
         render: (_: string, record: any) => (
           <UserProfileInfo
-            full_name={`${record?.first_name} ${record?.middle_name} ${record?.last_name}`}
+            full_name={`${record?.first_name} ${record?.middle_name}`}
             photoUrl={record?.photoUrl}
             link={`/ws/parent-detail/${record?.id}`}
           />
         ),
       },
       {
-        title: "Gender",
-        dataIndex: "gender",
-        key: "gender",
-        render: (val: string) => <span className="text-sm">{val || "-"}</span>,
+        title: "Sex",
+        dataIndex: "sex",
+        key: "sex",
+        render: (val: Sex) => get_formatted_sex(val),
       },
-
       {
         title: "Phone #",
         dataIndex: "phone",
