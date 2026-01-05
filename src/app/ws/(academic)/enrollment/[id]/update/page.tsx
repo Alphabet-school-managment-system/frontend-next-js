16824528c24080d9add78b6f41462844ad640999
surafelhabte1 <surafelhabte1@gmail.com>
2026-01-05 15:33:02 +0300

    feat: enhance enrollment form with validation and loading states, update student detail display

--- src/app/ws/(academic)/enrollment/[id]/update/page.tsx
+++ src/app/ws/(academic)/enrollment/[id]/update/page.tsx
@@ -8,6 +8,7 @@ import dynamic from "next/dynamic";
 import { FormSkeleton } from "@/components/forms/FormSkeleton";
 import { useEffect, useState } from "react";
 import { EnrollmentStudentDetail } from "../../new/page";
+import { Alert, Form } from "antd";
 
 const FormGenerator = dynamic(
   () => import("@/components/forms/FormGenerator"),
@@ -19,10 +20,34 @@ const FormGenerator = dynamic(
 
 export default function Update() {
   const { id } = useParams();
-  const { getFormFields } = useEnrollment();
+  const [form] = Form.useForm();
+
+  const {
+    getFormFields,
+    AcademicYear,
+    gettingAcademicYear,
+    SchoolSetting,
+    gettingSchoolSetting,
+    ServerDate,
+    gettingServerDate,
+    isEnrollmentPeriodExpired,
+  } = useEnrollment();
   const [data, setData] = useState<any>(null);
   const [student, setStudent] = useState<Student>();
-  const [enrolled_class, setEnrolledClass] = useState<string>("");
+  const [enrolled_grade, setEnrolledClass] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>();
+  const [enrollmentExpiredMessage, setEnrollmentExpiredMessage] = useState<
+    string | undefined
+  >();
+  const [disableForm, setDisableForm] = useState<boolean>(false);
+  useEffect(() => {
+    if (AcademicYear && ServerDate) {
+      const result = isEnrollmentPeriodExpired();
+
+      setEnrollmentExpiredMessage(result?.message);
+      // setDisableForm(result?.status);
+    }
+  }, [AcademicYear, ServerDate]);
 
   const { data: result, isLoading } = useApiQuery<Enrollment>(
     [],
@@ -31,9 +56,26 @@ export default function Update() {
   );
 
   useEffect(() => {
+    if (
+      isLoading ||
+      gettingAcademicYear ||
+      gettingServerDate ||
+      gettingSchoolSetting
+    ) {
+      setLoading(true);
+    } else {
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
+    }
+  }, [isLoading, gettingAcademicYear, gettingServerDate, gettingSchoolSetting]);
+
+  useEffect(() => {
     if (result) {
       const payload: any = { ...result };
       setData(payload);
+      setStudent(result?.student);
+      setEnrolledClass(result?.grade);
     }
   }, [result]);
 
@@ -42,21 +84,44 @@ export default function Update() {
       <FormGenerator
         columns={1}
         fields={getFormFields({
-          onStudentSelect: async (student) => {
+          onStudentSelect: async (student: Student) => {
             setStudent(student);
+            form.setFieldValue("student_id", student?.id);
           },
+          levels_of_education: SchoolSetting?.levels_of_education ?? [],
+          onGradeSelect: async (grade: string) => {
+            setEnrolledClass(grade);
+          },
+          onClear: async () => {
+            setEnrolledClass("");
+            setStudent(undefined);
+            form.setFieldValue("student_id", undefined);
+          },
+          isTransferredValue: result?.isTransferred,
         })}
         title="Update Enrollment Information"
         apiRoute="enrollment"
         data={data}
-        isFetching={isLoading}
         isCreate={false}
         leftContent={
           <EnrollmentStudentDetail
             student={student}
-            enrolled_class={enrolled_class}
+            enrolled_grade={enrolled_grade}
           />
         }
+        topContent={
+          enrollmentExpiredMessage ? (
+            <Alert
+              message="Enrollment Period Expired"
+              description={enrollmentExpiredMessage}
+              type="error"
+              showIcon
+            />
+          ) : undefined
+        }
+        isFetching={loading}
+        disableForm={disableForm}
+        formInstance={form}
       />
     </div>
   );
--- src/app/ws/(academic)/enrollment/new/page.tsx
+++ src/app/ws/(academic)/enrollment/new/page.tsx
@@ -6,9 +6,9 @@ import { useEnrollment } from "../hook/useEnrollment";
 import { Student } from "@/types";
 import Image from "next/image";
 import { staticImages } from "@/lib/static-images";
-import { useContext, useState } from "react";
-import { Icon } from "@iconify-icon/react";
+import { useContext, useEffect, useState } from "react";
 import { IdsContext } from "@/store/idsContext";
+import { Alert, Descriptions, Empty, Form } from "antd";
 
 const FormGenerator = dynamic(
   () => import("@/components/forms/FormGenerator"),
@@ -20,77 +20,122 @@ const FormGenerator = dynamic(
 
 export const EnrollmentStudentDetail = ({
   student,
-  enrolled_class,
+  enrolled_grade,
 }: {
   student?: Student;
-  enrolled_class: string;
+  enrolled_grade?: string;
 }) => {
+  const defaultPhoto =
+    student?.sex === "Female"
+      ? staticImages?.noPhotoGirlImg
+      : staticImages?.noPhotoBoyImg;
+
+  if (!student || !enrolled_grade) {
+    return (
+      <div className="flex justify-center items-center p-6">
+        <Empty description="Student details will appear here." />
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center rounded-md p-4 bg-gray-50 m-4 h-full">
-      {student ? (
-        <div
-          className={`flex flex-col justify-center items-center transition-opacity duration-300 ${
-            !student ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          <Image
-            src={
-              student?.photo_url ?? student?.gender === "Female"
-                ? staticImages.noPhotoGirlImg
-                : staticImages.noPhotoBoyImg
-            }
-            alt="Student image"
-            width={150}
-            height={150}
-            className="w-[150px] h-[150px] rounded-full"
-            priority
-          />
+    <div className="bg-gray-50 rounded-md p-6 shadow-sm m-4 max-w-md">
+      <div className="flex justify-center mb-4">
+        <Image
+          src={student?.image ?? defaultPhoto}
+          alt="Student image"
+          width={80}
+          height={80}
+          className="w-[150px] h-[150px] rounded-full object-cover mb-4"
+          priority
+        />
+      </div>
 
-          <h2 className="text-xl font-semibold">
-            {`${student?.first_name} ${student?.last_name}`}
-          </h2>
+      <h2 className="text-center text-base font-bold text-gray-900 uppercase underline mb-4">
+        {`${student.first_name} ${student.middle_name ?? ""} ${
+          student.last_name
+        }`}
+      </h2>
 
-          <p className="text-gray-600 text-md mt-2">
-            Enrolled Class:{" "}
-            <span className="font-medium">{enrolled_class}</span>
-          </p>
-        </div>
-      ) : (
-        <div
-          className={`flex flex-col justify-center items-center transition-opacity duration-300 ${
-            !student ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          <span className="flex items-center mb-2">
-            <Icon
-              icon="carbon:information"
-              width={30}
-              height={30}
-              className="text-gray-600"
-            />
-          </span>
-          <h2 className="text-lg font-medium mb-4 text-gray-600">
-            Student detail shown here.
-          </h2>
-        </div>
-      )}
+      <Descriptions column={1} layout="horizontal" size="middle" bordered>
+        <Descriptions.Item label="Grade">
+          {enrolled_grade}
+        </Descriptions.Item>
+
+        <Descriptions.Item label="Email">{student.email}</Descriptions.Item>
+
+        <Descriptions.Item label="Phone">{student.phone}</Descriptions.Item>
+
+        <Descriptions.Item label="Sex">{student.sex}</Descriptions.Item>
+
+        <Descriptions.Item label="Address">{student.address}</Descriptions.Item>
+
+        <Descriptions.Item label="Registration No">
+          {student.student_registration_number}
+        </Descriptions.Item>
+      </Descriptions>
     </div>
   );
 };
 
 export default function Home() {
   const { Ids } = useContext(IdsContext);
-  const { getFormFields } = useEnrollment();
+  const [form] = Form.useForm();
+
+  const {
+    getFormFields,
+    AcademicYear,
+    gettingAcademicYear,
+    SchoolSetting,
+    gettingSchoolSetting,
+    ServerDate,
+    gettingServerDate,
+    isEnrollmentPeriodExpired,
+  } = useEnrollment();
   const [student, setStudent] = useState<Student>();
-  const [enrolled_class, setEnrolledClass] = useState<string>("");
+  const [enrolled_grade, setEnrolledClass] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>();
+  const [enrollmentExpiredMessage, setEnrollmentExpiredMessage] = useState<
+    string | undefined
+  >();
+  const [disableForm, setDisableForm] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (AcademicYear && ServerDate) {
+      const result = isEnrollmentPeriodExpired();
+
+      setEnrollmentExpiredMessage(result?.message);
+      setDisableForm(result?.status);
+    }
+  }, [AcademicYear, ServerDate]);
+
+  useEffect(() => {
+    if (gettingAcademicYear || gettingServerDate || gettingSchoolSetting) {
+      setLoading(true);
+    } else {
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
+    }
+  }, [gettingAcademicYear, gettingServerDate, gettingSchoolSetting]);
 
   return (
     <div className="">
       <FormGenerator
         columns={1}
         fields={getFormFields({
-          onStudentSelect: async (student) => {
+          onStudentSelect: async (student: Student) => {
             setStudent(student);
+            form.setFieldValue("student_id", student?.id);
+          },
+          levels_of_education: SchoolSetting?.levels_of_education ?? [],
+          onGradeSelect: async (grade: string) => {
+            setEnrolledClass(grade);
+          },
+          onClear: async () => {
+            setEnrolledClass("");
+            setStudent(undefined);
+            form.setFieldValue("student_id", undefined);
           },
         })}
         title="Create new enrollment"
@@ -101,9 +146,22 @@ export default function Home() {
         leftContent={
           <EnrollmentStudentDetail
             student={student}
-            enrolled_class={enrolled_class}
+            enrolled_grade={enrolled_grade}
           />
         }
+        topContent={
+          enrollmentExpiredMessage ? (
+            <Alert
+              message="Enrollment Period Expired"
+              description={enrollmentExpiredMessage}
+              type="error"
+              showIcon
+            />
+          ) : undefined
+        }
+        isFetching={loading}
+        disableForm={disableForm}
+        formInstance={form}
       />
     </div>
   );
